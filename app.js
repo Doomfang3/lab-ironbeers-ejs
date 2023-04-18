c1c76873bad8b5d4a9293d2ef442e83314384715
ahmed awad <ahmeedsauce7@gmail.com>
2023-04-18 17:42:15 +0200

    Solved Mandatory Iterations

--- app.js
+++ app.js
@@ -19,4 +19,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Beers Route 
+app.get('/beers', (req, res) => { 
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {console.log('Beers from the database: ', beersFromApi)
+  res.render('beers', {beersFromApi});})
+  .catch(error => console.log(error));
+});
+
+// Random Beer Route 
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => { console.log('Beers from the database: ', responseFromAPI)
+  res.render('random-beer.ejs', {responseFromAPI});})
+  .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
